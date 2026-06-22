2247882f1309157f3cccad126b0154e953d7ab24
KaIawinka <standofferm555@gmail.com>
2026-06-22 16:28:34 +0600

    v0.6

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ import Forum from './Forum'
 import Footer from './Footer'
 
 const sales = [
-  { id: 1, img: '/product-1.jpg', title: '101 красная роза',         price: 2280, oldPrice: 2800, discount: 19, isTop: true },
-  { id: 2, img: '/product-2.jpg', title: '25 розовых пионовидных роз',price: 1680, oldPrice: 2800, discount: 10, isTop: true },
-  { id: 3, img: '/product-3.jpg', title: 'Влюбленность',              price: 1270, oldPrice: 2800, discount: 19, isTop: true },
-  { id: 4, img: '/product-4.jpg', title: 'Корзина все для тебя',      price: 2020, oldPrice: 2800, discount: 19, isTop: true },
+  { id: 1, img: '/product-1.jpg', title: '101 красная роза',           price: 2280, oldPrice: 2800, discount: 19, isTop: true },
+  { id: 2, img: '/product-2.jpg', title: '25 розовых пионовидных роз', price: 1680, oldPrice: 2800, discount: 10, isTop: true },
+  { id: 3, img: '/product-3.jpg', title: 'Влюбленность',               price: 1270, oldPrice: 2800, discount: 19, isTop: true },
+  { id: 4, img: '/product-4.jpg', title: 'Корзина все для тебя',       price: 2020, oldPrice: 2800, discount: 19, isTop: true },
 ]
 
 const seasonal = [
-  { id: 5, img: '/product-5.jpg', title: 'Мечтательница',             price: 2280, isTop: true },
+  { id: 5, img: '/product-5.jpg', title: 'Мечтательница',                 price: 2280, isTop: true },
   { id: 6, img: '/product-6.jpg', title: 'Корзина солнечного настроения', price: 1680, isTop: true },
-  { id: 7, img: '/product-7.jpg', title: 'Цветочная клумба',          price: 1270, isTop: true },
-  { id: 8, img: '/product-8.jpg', title: 'Корзина Дюймовочка',        price: 2020, isTop: true },
+  { id: 7, img: '/product-7.jpg', title: 'Цветочная клумба',              price: 1270, isTop: true },
+  { id: 8, img: '/product-8.jpg', title: 'Корзина Дюймовочка',            price: 2020, isTop: true },
 ]
 
 const gifts = [
